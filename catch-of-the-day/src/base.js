00427cb7d1f55ead18dd2dcc31b74e4cbd6d898c
Dominique Alexander <dale2405@students.vsu.edu>
2017-01-02 12:42:56 -0500

    Add lesson 20

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,8 @@
+import Rebase from 're-base';
+const base = Rebase.createClass({
+  apiKey: "AIzaSyCBbImHgq9B_jSLXe9IgPPk2Zxc49GjRxI",
+  authDomain: "catch-of-the-day-480e4.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-480e4.firebaseio.com"
+});
+
+export default base;
--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -1,10 +1,50 @@
 import React from 'react';
 import AddFishForm from './AddFishForm'
+import {formatPrice} from '../helpers'
 class Inventory extends React.Component{
+  constructor(){
+    super();
+    this.renderInventory = this.renderInventory.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e,key){
+    const fish = this.props.fishes[key];
+    const updatedFish = {
+      ...fish,
+      [e.target.name]: e.target.value
+    }
+    this.props.updateFish(key,updatedFish)
+  }
+  renderInventory(key){
+    const fish = this.props.fishes[key];
+    return (
+      <div className="fish-edit" key={key}>
+        <input type="text" placeholder="Fish Name" value={fish.name}
+        onChange={(e) => this.handleChange(e,key)}/>
+        <input type="text" placeholder="Fish Price" value={formatPrice(fish.price)}
+        onChange={(e) => this.handleChange(e,key)}/>
+        <select value={fish.status}
+        onChange={(e) => this.handleChange(e,key)}>
+          <option value="available">Fresh!</option>
+          <option value="unavailable">Sold Out!</option>
+        </select>
+        <input type="text" placeholder="Fish Status"
+        onChange={(e) => this.handleChange(e,key)}/>
+
+        <textarea placeholder="Fish Desc" value={fish.desc}
+        onChange={(e) => this.handleChange(e,key)}></textarea>
+        <input type="text" placeholder="Fish Image" value={fish.image}
+        onChange={(e) => this.handleChange(e,key)}/>
+        <button onClick={() => this.props.removeFish(key)}>Remove Fish</button>
+      </div>
+    )
+  }
   render() {
     return (
       <div>
         <h2> Inventory </h2>
+        {Object.keys(this.props.fishes).map(this.renderInventory)}
+
         <AddFishForm addFish={this.props.addFish}/>
         <button onClick={this.props.loadSamples}>Load Sample Fishes</button>
       </div>
